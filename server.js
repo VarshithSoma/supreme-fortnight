18fc7c04fc8097529e9955b65aa44545b69990ad
varshithkumarsoma <varshithkumarsoma@gmail.com>
2025-06-08 20:11:03 +0530

    chore: add endpoint for contests

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 app.get("/", async (req, res) => {
   res.send("hello");
 });
+app.get("/contests", (req, res) => {
+  const dataFile = "contests_cache.json";
+  const rawData = fs.readFileSync(dataFile, "utf-8");
+  const data = JSON.parse(rawData);
+  res.json(data);
+});
 app.listen(3000, () => {
   console.log("server running at port 3000");
 });
